489401a21e92c4e6ce8a40051cb6fc2c23bb3243
kr1stjans <kristjansesek@gmail.com>
2017-09-09 14:38:50 +0200

    GA fix

--- js/main.js
+++ js/main.js
@@ -17,4 +17,12 @@ $(document).ready(function () {
         mac.addClass('my-btn2');
         ubuntu.addClass('my-btn');
     }
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-106176253-1', 'auto');
+    ga('send', 'pageview', '/synonymful');
 });

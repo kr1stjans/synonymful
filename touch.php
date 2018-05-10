98214ab2309c023ad362e396829fb930cd01e3c9
jarheadSLO <klemen1111@gmail.com>
2018-05-10 23:58:04 +0200

    Update

--- touch.php
+++ touch.php
@@ -1,14 +1,19 @@
 <?php
     $currentVersion = trim(file_get_contents('version.txt'));
     $version = isset($_GET["version"]) ? $_GET["version"] : "";
+    $os = isset($_GET["os"]) ? $_GET["os"] : "";
 
-    if (!isset($version) || strlen($version) <= 0 || $version == $currentVersion) {
+    if (!isset($version) || !isset($os) || strlen($version) <= 0 || $version == $currentVersion) {
         header("HTTP/1.1 204 NO CONTENT");
     } else {
         header('Content-type: application/json');
         $actualLink = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+        $latestPath = array(
+            "darwin" => "/latest/darwin/Synonymful.zip",
+            "linux" => "/latest/linux/Synonymful.zip",
+        );
         echo json_encode(array(
-            "url" => $actualLink . "/latest/darwin/Synonymful.zip",
+            "url" => $actualLink . $latestPath[$os],
         ));
     }
 ?>
